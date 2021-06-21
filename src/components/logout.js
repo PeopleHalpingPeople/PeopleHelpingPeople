256e614fcbf7c042a037b143dfda4463fdc82358
Jason Taisey <jdtaisey89@icloud.com>
2021-06-21 11:38:27 -0700

    got the login/logout screen to work within the application

--- src/components/logout.js
+++ src/components/logout.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import Button from '@material-ui/core/Button';
-// function Logout() {
-//   const {
-//     isAuthenticated,
-//     logout,
-//   } = useAuth0();
-//   return isAuthenticated && (
-//     <Button variant="contained" onClick={() => {
-//       logout({ returnTo: window.location.origin });
-//     }}>Log out</Button>
-//   );
-// }
+
 function Logout() {
-  return (
-    <>
-      <Button>Logout</Button>
-    </>
-  )
+  const {
+    isAuthenticated,
+    logout,
+  } = useAuth0();
+  return isAuthenticated && (
+    <Button variant="contained" onClick={() => {
+      logout({ returnTo: window.location.origin });
+    }}>Log out</Button>
+  );
 }
+// function Logout() {
+//   return (
+//     <>
+//       <Button>Logout</Button>
+//     </>
+//   )
+// }
 export default Logout;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app.js";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-function Entry () {
+function Entry() {
   return (
     <>
-      <App/>
+      <Auth0Provider
+        domain="simoneodegard.us.auth0.com"
+        clientId="0urVRAetVyyupPCix7ONfM0bwxXD2FB5"
+        redirectUri={window.location.origin}
+      >
+        <App />
+      </Auth0Provider>
     </>
-  )
+  );
 }
 
-const root = document.getElementById('root');
-ReactDOM.render(<Entry/>, root);
+const root = document.getElementById("root");
+ReactDOM.render(<Entry />, root);
